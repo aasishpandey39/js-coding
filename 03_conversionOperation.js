e6df6d3dc991055c507afbbe4cf8f4b70e788489
Aasish Pandey <aasishpandeybgp@gmail.com>
2024-05-25 21:55:27 +0530

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+//let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+//"33" easily convert in =>33
+//"33abc"=>NaN
+//true=>1 ; false=>0
+
+let isLoggedIn = "Ashish"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoogedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"Ashish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
