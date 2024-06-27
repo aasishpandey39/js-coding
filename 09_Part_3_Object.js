a1a9aaf2b2b631ba2ef9bc4d2f40d6df49c236fd
Aasish Pandey <aasishpandeybgp@gmail.com>
2024-06-27 18:54:50 +0530

    object Part 3

---
+++ 09_Part_3_Object.js
@@ -0,0 +1,41 @@
+const course = {
+    coursename: "Ashish in english",
+    price: "999",
+    courseInstructor: "Ashish"
+}
+
+// course.courseInstructor
+const{courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// react concept -> ({company}) it means destructuring of object
+/* const navbar = ({company}) => {
+
+}
+navbar(company = 'Ashish')  */
+
+
+// API -> 
+/* An API, or Application Programming Interface, is a set of rules 
+and protocols that allows different software applications to communicate with each other. */
+
+
+// JSON(javascript object notation) format
+// API call
+// API found in object
+
+// {
+//  "name": "Ashish",
+//  "coursename": " js in english",
+//  "price": "free",
+//  }
+
+// API also found in array formet, object inside array
+[
+    {},
+    {},
+    {},
+]
