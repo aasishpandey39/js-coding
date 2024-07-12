58134dc429153fc3b737f2e7783e7b3cb46e522c
Aasish Pandey <aasishpandeybgp@gmail.com>
2024-07-12 13:36:58 +0530

    Loops

---
+++ 23_Loop6.js
@@ -0,0 +1,32 @@
+// const coding = [ "js", "rb", "java", "cpp","python"]
+
+
+
+// // store in variable
+
+// const values = coding.forEach ( (item) => {
+//    // console.log(item);
+//     return item  //forEach does't return the value
+// } )
+
+// console.log(values);
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => { 
+//     return num > 4  // when you open the scope return keyword is necessary.
+//  })
+
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+
+// console.log(newNums);
+
