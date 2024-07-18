6701f5b548933e131f4d2f9863132cdd31e4fdf6
Aasish Pandey <aasishpandeybgp@gmail.com>
2024-07-18 15:08:09 +0530

    (DOM) theory.js

---
+++ 26_DOM.js
@@ -0,0 +1,27 @@
+/* => Document Object Model (DOM) in JavaScript
+
+The Document Object Model (DOM) is a programming interface for web documents. 
+It represents the page so that programs can change the document structure, style, and content. 
+The DOM represents the document as a tree of objects, with each node representing part of the document,
+such as elements, attributes, and text.
+
+=>> Key Concepts of the DOM
+
+=> Nodes and Elements:
+
+Node: The basic unit of the DOM. Everything in the DOM is a node, including elements, text, attributes, comments, etc.
+Element: A type of node that represents HTML elements.
+Tree Structure:
+
+The DOM represents the document as a hierarchical tree structure.
+The root of the tree is the document object.
+Nodes in the tree can have parent and child relationships.
+DOM Methods:
+
+Methods to access and manipulate elements, attributes, and other nodes in the document.
+
+
+Summary
+The DOM is a powerful interface that allows developers to dynamically interact with and manipulate web documents
+.Using JavaScript, you can access elements, change their content, attributes, and styles, and handle user events
+ to create dynamic and interactive web pages. Understanding the DOM is essential for modern web development.*/
